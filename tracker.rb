530748c051ce4d635b614a5d906bcce3063b79bf
gazelle <myvars@gmail.com>
2008-11-18 18:32:34 -0700

    Added caching, scrape, whitelist

--- tracker.rb
+++ tracker.rb
@@ -12,6 +12,10 @@ require 'rubygems'
 require 'bencode'
 # Require the mysql gem
 require 'mysql'
+# Require the memcache gem
+require 'memcache'
+# Require our cache-abstraction class
+require 'cache'
 # Require the sinatra gem
 require 'sinatra'
 # Require YAML to parse config files
@@ -22,10 +26,10 @@ configure do
   $config = YAML::load( open('config.yml') )
   # Connect to MySQL
   $db = Mysql::new($config[:mysql][:host], $config[:mysql][:user], $config[:mysql][:pass], $config[:mysql][:database])
-
-  whitelist = $db.query( "SELECT Peer_ID FROM whitelist" )
+  
+  whitelist = $db.query( "SELECT Peer_ID, Client FROM whitelist" )
   $whitelist = Array.new
-  whitelist.each_hash { |client| $whitelist << /#{client['Peer_ID']}/ } # Put a RegEx of each peerid into $whitelist
+  whitelist.each_hash { |client| $whitelist << { :regex => /#{client['Peer_ID']}/, :name => client['Client'] } } # Put a RegEx of each peerid into $whitelist
 end
 
 get '/:passkey/announce' do
@@ -35,73 +39,72 @@ get '/:passkey/announce' do
     optional_params = ['event', 'compact', 'no_peer_id', 'ip', 'numwant']
     (required_params + optional_params).each do |param|
       error "Bad Announce" if (params[param].nil? or params[param].empty?) and required_params.include?(param)
-      self.instance_variable_set("@" + param, params[param])
+      self.instance_variable_set("@" + param, escape(params[param]))
     end
-    @numwant ||= 50
     @event ||= 'none'
+    @ip ||= escape(@ip)
+    @numwant ||= 50
     
     # Make sure client is whitelisted
-    whitelisted = $whitelist.map { |client| @peer_id =~ client }.include? 0
+    whitelisted = $whitelist.map { |client| @peer_id =~ client[:regex] }.include? 0
     error "Your client is banned. Go to #{$config[:whitelist_url]}" unless whitelisted
-
+    
+    # Instantiate a cache object for this request
+    cache = Cache.new(:host => $config[:cache][:host], :namespace => $config[:cache][:namespace], :passkey => @passkey, :peer_id => @peer_id, :info_hash => @info_hash)
+    
     # Find our user
-    user = $db.query( "SELECT um.ID, um.Enabled, um.can_leech, p.Level FROM users_main AS um LEFT JOIN permissions AS p ON um.PermissionID=p.ID WHERE torrent_pass = '#{escape(@passkey)}'" ).fetch_hash
-    error "Account not found" if user.nil? or user['Enabled'] != '1'
-    error "Leeching Disabled" if user['can_leech'] != '1' and @left != 0
+    user = cache.user
+    error "Account not found" unless user.exists?
+    error "Leeching Disabled" unless user.can_leech?
     
     # Find our torrent
-    torrent = $db.query( "SELECT t.ID, t.FreeTorrent FROM torrents AS t WHERE t.info_hash = '#{escape(@info_hash)}'" ).fetch_hash
-    error "Torrent not found" if torrent.nil?
+    torrent = cache.torrent
+    error "Torrent not found" unless torrent.exists?
     
     # Find peers
-    peers = $db.query( "SELECT p.UserID, p.IP, p.Port, p.PeerID FROM tracker_peers AS p WHERE TorrentID = '#{torrent['ID']}' ORDER BY RAND() LIMIT #{@numwant.to_i}" )
-    
+    peers = torrent.peers[0, @numwant]
+        
     # Log Announce
-    $db.query( "INSERT INTO tracker_announce_log (UserID, TorrentID, IP, Port, Event, Uploaded, Downloaded, tracker_announce_log.Left, PeerID, RequestURI, Time) VALUES (#{user['ID']}, #{torrent['ID']}, '#{escape request.env['REMOTE_ADDR']}', #{escape @port}, '#{escape @event}', #{escape @uploaded}, #{escape @downloaded}, #{escape @left}, '#{escape @peer_id}', '#{escape request.env['QUERY_STRING']}', NOW())" ) if $config[:log_announces]
+    $db.query( "INSERT INTO tracker_announce_log (UserID, TorrentID, IP, Port, Event, Uploaded, Downloaded, tracker_announce_log.Left, PeerID, RequestURI, Time) VALUES (#{user['ID']}, #{torrent['ID']}, '#{@ip}', #{@port}, '#{@event}', #{@uploaded}, #{@downloaded}, #{@left}, '#{@peer_id}', '#{escape request.env['QUERY_STRING']}', NOW())" ) if $config[:log_announces]
     
     # Generate Peerlist
-    peer_list = []
-    peer_list_compact = ''
-    peers.each_hash do |peer| # Go through each peer
-      if @compact == '1' # Compact Mode
+    if @compact == '1' # Compact Mode
+      peer_list = ''
+      peers.each do |peer| # Go through each peer
         ip = peer['IP'].split('.').collect { |octet| octet.to_i }.pack('C*')
         port = [peer['Port'].to_i].pack('n*')
-        peer_list_compact << ip + port
-      else # Normal Mode
+        peer_list << ip + port
+      end
+    else
+      peer_list = []
+      peers.each do |peer| # Go through each peer
         peer_hash = { 'ip' => peer['IP'], 'port' => peer['Port'] }
         peer_hash.update( { 'peer id' => peer['PeerID'] } ) unless @no_peer_id == 1
-         peer_list << peer_hash
+        peer_list << peer_hash
       end
     end
     
-    if @compact == '1' # Compact Mode
-      @resp = { 'interval' => $config[:announce_int], 'min interval' => $config[:min_announce_int], 'peers' => peer_list_compact }
-    else
-      @resp = { 'interval' => $config[:announce_int], 'min interval' => $config[:min_announce_int], 'peers' => peer_list }
-    end
+    @resp = { 'interval' => $config[:announce_int], 'min interval' => $config[:min_announce_int], 'peers' => peer_list }
     # End peerlist generation
     
-    # Update database
+    # Update database / cache
     # Update values specific to each event
     case @event
     when 'started'
       # Add the user to the torrents peerlist, then update the seeder / leecher count
-      $db.query( "INSERT INTO tracker_peers (UserID, TorrentID, IP, Port, Uploaded, Downloaded, tracker_peers.Left, PeerID) VALUES (#{user['ID']}, #{torrent['ID']}, '#{escape request.env['REMOTE_ADDR']}', '#{escape @port}', 0, 0, #{escape @left}, '#{escape @peer_id}')" )
+      torrent.new_peer(@ip, @port, @left, @peer_id)
       $db.query( "UPDATE torrents SET #{@left.to_i > 0 ? 'Leechers = Leechers + 1' : 'Seeders = Seeders + 1'} WHERE ID = #{torrent['ID']}" )
     when 'completed'
-      $db.query( "INSERT INTO tracker_snatches (UserID, TorrentID, IP, Port, Uploaded, Downloaded, PeerID) VALUES (#{user['ID']}, #{torrent['ID']}, '#{escape request.env['REMOTE_ADDR']}', #{escape @port}, #{escape @uploaded}, #{escape @downloaded}, '#{escape @peer_id}')" )
+      $db.query( "INSERT INTO tracker_snatches (UserID, TorrentID, IP, Port, Uploaded, Downloaded, PeerID) VALUES (#{user['ID']}, #{torrent['ID']}, '#{@ip}', #{@port}, #{@uploaded}, #{@downloaded}, '#{@peer_id}')" )
       $db.query( "UPDATE torrents SET Seeders = Seeders + 1, Leechers = Leechers - 1, Snatched = Snatched + 1 WHERE ID = #{torrent['ID']}" )
     when 'stopped'
       # Update Seeder / Leecher count for torrent, and update snatched list with final upload / download counts, then delete the user from the torrents peerlist
-      $db.query( "UPDATE torrents AS t, tracker_snatches AS s SET #{@left.to_i > 0 ? 't.Leechers = t.Leechers - 1' : 't.Seeders = t.Seeders - 1'}, s.Uploaded = #{escape @uploaded}, s.Downloaded = #{escape @downloaded} WHERE t.ID = #{torrent['ID']} AND (s.UserID = #{user['ID']} AND s.TorrentID = #{torrent['ID']})" )
-      $db.query( "DELETE FROM tracker_peers WHERE PeerID = '#{escape @peer_id}' AND TorrentID = #{torrent['ID']}" )
+      $db.query( "UPDATE torrents AS t, tracker_snatches AS s SET #{@left.to_i > 0 ? 't.Leechers = t.Leechers - 1' : 't.Seeders = t.Seeders - 1'}, s.Uploaded = #{@uploaded}, s.Downloaded = #{@downloaded} WHERE t.ID = #{torrent['ID']} AND (s.UserID = #{user['ID']} AND s.TorrentID = #{torrent['ID']})" )
+      torrent.delete_peer
     end
     
-    # Update uploaded / downloaded / left amounts
-    # This is two queries because we need the old p.Uploaded value to find out what they've uploaded since the last announce, and when it's one query, it uses the current uploaded amount, which is useless since @uploaded - @uploaded = 0, and the user's ratio would never update
-    $db.query( "UPDATE users_main AS u, tracker_peers AS p SET u.Uploaded=u.Uploaded+#{escape @uploaded}-p.Uploaded, u.Downloaded=u.Downloaded+#{escape @downloaded}-p.Downloaded WHERE (p.PeerID='#{escape @peer_id}' AND u.ID=#{user['ID']} AND p.TorrentID=#{torrent['ID']})" )
-    $db.query( "UPDATE tracker_peers AS p SET p.Uploaded = #{escape @uploaded}, p.Downloaded = #{escape @downloaded}, p.Left = #{escape @left} WHERE p.PeerID = '#{escape @peer_id}' AND TorrentID = #{torrent['ID']}" )
-    # End database update
+    # Add user to the update queue
+    user.update(@uploaded, @downloaded, @left)
     
     @resp.bencode
   rescue TrackerError => e
@@ -110,7 +113,15 @@ get '/:passkey/announce' do
 end
 
 get '/:passkey/scrape' do
-  {}.bencode
+  error "Bad Scrape" if params['info_hash'].nil? or params['info_hash'].empty?
+  cache = MemCache::new $config[:cache][:host], :namespace => $config[:cache][:namespace]
+  torrent = Cache::Torrent.new(cache, $db, params['info_hash'])
+  
+  { params['info_hash'] => { 'complete' => torrent['Seeders'], 'incomplete' => torrent['Leechers'], 'downloaded' => torrent['Snatched'], 'name' => torrent['Name'] } }.bencode
+end
+
+get '/whitelist' do
+  $whitelist.collect { |client| client[:name] }.join('<br />')
 end
 
 helpers do
@@ -122,5 +133,5 @@ helpers do
   end
 end
 
-# Error Class
-class TrackerError < RuntimeError; end
+# Error class
+class TrackerError < RuntimeError; end # Used to clearly identify errors
